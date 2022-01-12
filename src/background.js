c596680dfc2c0845c17e0cf5eb0cf3b3eac268f7
Brian Chen <brian.chen@tabcorp.com.au>
2022-01-13 09:21:23 +1100

    able to find defs related to package

--- src/background.js
+++ src/background.js
@@ -3,11 +3,35 @@ const { Octokit } = require('@octokit/rest');
 
 const octokit = new Octokit();
 
-console.log(octokit);
+const pathPrefix = '/package/';
+const {
+  scope,
+  packageName,
+} = (() => {
+  const split = window.location.pathname.substring(pathPrefix.length).split('/');
 
-type A = {
+  if (split.length > 1) {
+    return {
+      scope: split[0],
+      packageName: split[1],
+    }
+  }
 
-}
-let color = '#3aa757';
+  return {
+    scope: undefined,
+    packageName: split[0],
+  }
+})();
 
-console.log(color);
+octokit.repos.getContent({
+  owner: 'flow-typed',
+  repo: 'flow-typed',
+  path: `definitions/npm${scope ? `/${scope}` : ''}`,
+}).then((res) => {
+  const filteredDefinitions = res.data.filter((defs) => defs.name.startsWith(`${packageName}_`));
+
+  if (filteredDefinitions.length > 0) {
+    // do something
+    console.log(filteredDefinitions);
+  }
+});
